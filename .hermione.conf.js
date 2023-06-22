f0c1713e18566d17684e9717641eacaa9311e6bb
ars-sobolev <ars-sobolev@yandex-team.ru>
2023-06-22 18:57:44 +0500

    add sessionsPerBrowser and multiply tests

--- .hermione.conf.js
+++ .hermione.conf.js
@@ -1,15 +1,16 @@
 module.exports = {
     gridUrl: 'http://localhost:4444/wd/hub',
+    sessionsPerBrowser: 5,
     browsers: {
         chrome: {
             desiredCapabilities: {
                 browserName: 'chrome'
-            }
+            },
         },
         chrome2:  {
             desiredCapabilities: {
                 browserName: 'chrome'
-            }
+            },
         },
     },
     plugins: {
--- tests/test.hermione.js
+++ tests/test.hermione.js
@@ -4,3 +4,23 @@ hermione.only.in(['chrome', 'chrome2']);
 it('should pass', async function() {
     assert.equal(1, 1);
 });
+
+it('should pass 2', async function() {
+    assert.equal(1, 1);
+});
+
+it('should pass 3', async function() {
+    assert.equal(1, 1);
+});
+
+it('should pass 4', async function() {
+    assert.equal(1, 1);
+});
+
+it('should pass 5', async function() {
+    assert.equal(1, 1);
+});
+
+it('should pass 6', async function() {
+    assert.equal(1, 1);
+});
