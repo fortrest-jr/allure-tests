49bf70afd66c3b9b489f38bb0325368d88c240c3
Alena Makarova <annamolly888@yandex.ru>
2023-08-01 10:56:11 +0300

    change link

--- tests/test.hermione.js
+++ tests/test.hermione.js
@@ -2,29 +2,21 @@ const { assert } = require("chai");
 
 it("fail assertView", async ({ browser, currentTest }) => {
   const items = await browser.$$(".serp-item_card-has-warning");
-  await browser.url("https://ya.ru/");
+  await browser.url("https://www.startpage.com/");
 
   await browser.step(currentTest.id(), "делаем раз", async () => {
-    await browser.$(".search3__input").setValue("hello world");
+    await browser.$("#q").setValue("hello world");
   });
 
   await browser.step(currentTest.id(), "скриншотим кнопку", async () => {
-    await browser.assertView('plain', '.search3__button');
+    await browser.assertView("plain", "#search-btn");
   });
 
   await browser.step(currentTest.id(), "жмяк на кнопку поиска", async () => {
-    await browser.$(".search3__button").click();
+    await browser.$("#search-btn").click();
   });
 
   await browser.step(currentTest.id(), "не падает ассерт", async () => {
     assert.equal(items.length, +0);
   });
-
-  await browser.step(
-    currentTest.id(),
-    "на странице есть ворнинги",
-    async () => {
-      await browser.$(".WikiArticle").isExisting();
-    }
-  );
 });
