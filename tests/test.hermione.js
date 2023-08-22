cff99003bf69ea53417e19d936ae129c1af8095e
Alena Makarova <annamolly888@yandex.ru>
2023-08-22 14:31:53 +0300

    add new problem

--- tests/test.hermione.js
+++ tests/test.hermione.js
@@ -1,22 +1,73 @@
 const { assert } = require("chai");
 
-it("fail assertView", async ({ browser, currentTest }) => {
+it("assertView", async ({ browser, currentTest }) => {
   const items = await browser.$$(".serp-item_card-has-warning");
   await browser.url("https://www.startpage.com/");
 
-  await browser.step(currentTest.id(), "делаем раз", async () => {
+  await browser.step(currentTest.id, "делаем раз", async () => {
     await browser.$("#q").setValue("hello world");
   });
 
-  await browser.step(currentTest.id(), "скриншотим кнопку", async () => {
+  await browser.step(currentTest.id, "скриншотим кнопку", async () => {
     await browser.assertView("plain", "#search-btn");
   });
 
-  await browser.step(currentTest.id(), "жмяк на кнопку поиска", async () => {
+  await browser.step(currentTest.id, "жмяк на кнопку поиска", async () => {
     await browser.$("#search-btn").click();
   });
 
-  await browser.step(currentTest.id(), "не падает ассерт", async () => {
+  await browser.step(currentTest.id, "не падает ассерт", async () => {
+    assert.equal(items.length, +0);
+  });
+});
+
+it("2", async ({ browser, currentTest }) => {
+  const items = await browser.$$(".serp-item_card-has-warning");
+  await browser.url("https://www.startpage.com/");
+
+  await browser.step(currentTest.id, "делаем раз", async () => {
+    await browser.$("#q").setValue("world");
+  });
+
+  await browser.step(currentTest.id, "жмяк на кнопку поиска", async () => {
+    await browser.$("#search-btn").click();
+  });
+
+  await browser.step(currentTest.id, "не падает ассерт", async () => {
+    assert.equal(items.length, +0);
+  });
+});
+
+it("3", async ({ browser, currentTest }) => {
+  const items = await browser.$$(".serp-item_card-has-warning");
+  await browser.url("https://www.startpage.com/");
+
+  await browser.step(currentTest.id, "делаем раз", async () => {
+    await browser.$("#q").setValue("world");
+  });
+
+  await browser.step(currentTest.id, "жмяк на кнопку поиска", async () => {
+    await browser.$("#search-btn").click();
+  });
+
+  await browser.step(currentTest.id, "не падает ассерт", async () => {
+    assert.equal(items.length, +0);
+  });
+});
+
+it("4", async ({ browser, currentTest }) => {
+  const items = await browser.$$(".serp-item_card-has-warning");
+  await browser.url("https://www.startpage.com/");
+
+  await browser.step(currentTest.id, "делаем раз", async () => {
+    await browser.$("#q").setValue("world");
+  });
+
+  await browser.step(currentTest.id, "жмяк на кнопку поиска", async () => {
+    await browser.$("#search-btn").click();
+  });
+
+  await browser.step(currentTest.id, "не падает ассерт", async () => {
     assert.equal(items.length, +0);
   });
 });
